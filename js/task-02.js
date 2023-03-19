43c7fce8b20f8adcc1680dfbddf957d278f00ec2
Eissberg <jeka.chk@gmail.com>
2023-03-19 18:08:08 +0200

    second ready

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let listOfLi = [];
+
+for (let item of ingredients) {
+  const addLi = document.createElement('li');
+  addLi.textContent = item;
+  addLi.classList.add('item');
+  listOfLi.push(addLi);
+}
+
+const ulForAdd = document.querySelector('ul#ingredients');
+
+listOfLi.forEach((item) => {
+  ulForAdd.append(item);
+});
